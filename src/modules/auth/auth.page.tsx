7cde80ef2c0400cd4ad08f7f7cd6cd6e1779da04
ujangaripin24 <ujangarifin06@gmail.com>
2026-01-14 08:59:59 +0700

    crousel login page [done]

--- src/modules/auth/auth.page.tsx
+++ src/modules/auth/auth.page.tsx
@@ -1,12 +1,40 @@
 import { Button, Checkbox, Form, Image, Input } from '@heroui/react'
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import Logo from "../../assets/assets-s2re.svg"
 import { EyeFilledIcon, EyeSlashFilledIcon } from '../../components/Icons'
+import Image1 from './../../assets/carousel/carousel-1.png'
+import type { EmblaCarouselType } from "embla-carousel"
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
+
+const IMAGES = [
+  Image1,
+  Image1,
+  Image1,
+];
 
 const AuthPage: React.FC = () => {
   const [isVisible, setIsVisible] = useState(false);
-
   const toggleVisibility = () => setIsVisible(!isVisible);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ delay: 3000, stopOnInteraction: false })
+  ]);
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+    emblaApi.on("select", onSelect);
+    emblaApi.on("reInit", onSelect);
+    return () => {
+      emblaApi.off("select", onSelect);
+      emblaApi.off("reInit", onSelect);
+    };
+  }, [emblaApi, onSelect]);
   return (
     <div className="grid grid-cols-1 lg:grid-cols-12 gap-0 min-h-screen bg-[#2D68A2]">
       <div className="bg-[#fff] flex items-center justify-center lg:col-span-5 lg:rounded-r-[16px] z-10">
@@ -62,7 +90,45 @@ const AuthPage: React.FC = () => {
 
       <div className="bg-[#2D68A2] hidden lg:flex items-center lg:col-span-7 justify-center">
         <div className="flex flex-col items-center justify-center w-full px-8 gap-y-4">
-          Crousel
+          <div className='flex flex-col items-center w-full gap-y-6'>
+            <div className="w-full overflow-hidden rounded-xl" ref={emblaRef}>
+
+              <div className="flex">
+                {IMAGES.map((src, index) => (
+                  <div
+                    key={index}
+                    className="relative flex-[0_0_100%] min-w-0 h-64 md:h-[400px]"
+                  >
+                    <Image
+                      src={src}
+                      alt={`Slide ${index + 1}`}
+                      className="object-contain w-full h-full"
+                      radius="none"
+                      removeWrapper
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="flex justify-center space-x-3 mt-4">
+              {IMAGES.map((_, index) => (
+                <button
+                  key={index}
+                  onClick={() => emblaApi && emblaApi.scrollTo(index)}
+                  className={`w-3 h-3 rounded-full transition-all duration-300 ${selectedIndex === index ? "bg-black/50 scale-125" : "bg-white/50"
+                    }`}
+                  aria-label={`Go to slide ${index + 1}`}
+                />
+              ))}
+            </div>
+
+
+            <div className="text-center text-white">
+              <h2 className="text-[24px] font-semibold">Building Happiness, Shaping Futures</h2>
+              <p className="text-sm opacity-90">Where joy meets learning, and dreams take flight...</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
