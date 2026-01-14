a424c3591086d6ba16351bebbd2c5d29171f3f27
ujangaripin24 <ujangarifin06@gmail.com>
2026-01-14 15:37:10 +0700

    add new connection for connection between flow

--- src/modules/flow/flow.type.ts
+++ src/modules/flow/flow.type.ts
@@ -1,13 +1,17 @@
-import type { Node, Edge } from "reactflow";
+import type { Node, Edge, Connection } from "reactflow";
 
 export type FlowState = {
   nodes: Node[];
   edges: Edge[];
   selectedNodeId?: string;
+  selectedEdgeId?: string;
 
   addProcessorAtPosition: (x: number, y: number) => void;
   onNodesChange: (changes: any) => void;
   onEdgesChange: (changes: any) => void;
+  onConnect: (connection: Connection) => void;
+
   setSelectedNode: (id?: string) => void;
-  deleteSelectedNode: () => void;
+  setSelectedEdge: (id?: string) => void;
+  deleteSelected: () => void;
 };
